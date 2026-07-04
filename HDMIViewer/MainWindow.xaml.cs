e025e1c9c3e94285dd05df4a7954459bb1163e3d
youyou005a <11438188+youyou005a@user.noreply.gitee.com>
2026-07-04 11:52:59 +0800

    添加项目文件。

---
+++ HDMIViewer/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using DirectShowLib;
+
+namespace HDMIViewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Scan_Click(object sender, RoutedEventArgs e)
+        {
+            DeviceList.Items.Clear();
+
+            try
+            {
+                var devices = DsDevice.GetDevicesOfCat(FilterCategory.VideoInputDevice);
+
+                foreach (var d in devices)
+                {
+                    DeviceList.Items.Add(d.Name);
+                }
+            }
+            catch (System.Exception ex)
+            {
+                DeviceList.Items.Add("错误：" + ex.Message);
+            }
+        }
+    }
+}
